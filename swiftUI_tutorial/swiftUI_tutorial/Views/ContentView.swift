49a144240a45f448a180399266fa2cbc00499ae2
feldblume5263 <feldblume5263@gmail.com>
2021-11-07 16:37:52 +0900

    update swift ui tutorial part2

---
+++ swiftUI_tutorial/swiftUI_tutorial/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUI_tutorial
+//
+//  Created by Junhong Park on 2021/11/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
