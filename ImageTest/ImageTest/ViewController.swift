bed4bf6fc0a3dbe2e4b0b27f2668feaeb9cfffa9
feldblume5263 <feldblume5263@gmail.com>
2021-11-09 19:01:59 +0900

    complete image test

--- ImageTest/ImageTest/ViewController.swift
+++ ImageTest/ImageTest/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var testImage: UIImageView!
+    @IBOutlet weak var testButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setTestImage()
+        setTestButton()
     }
-
-
+    
+    func setTestButton() {
+        
+        testButton.backgroundColor = .clear
+        testButton.setTitle("___", for: .normal)
+        testButton.addTarget(self, action: #selector(changeImageColor), for: .touchUpInside)
+    }
+    
+    func setTestImage() {
+        
+        testImage.backgroundColor = .clear
+        testImage.image = UIImage(named: "circle2")?.withRenderingMode(.alwaysTemplate)
+        testImage.tintColor = .blue
+    }
+    
+    @objc func changeImageColor() {
+        
+        if testImage.tintColor == .blue {
+            testImage.tintColor = .black
+        } else {
+            testImage.tintColor = .blue
+        }
+    }
 }
 
