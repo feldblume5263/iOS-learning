36043a7cdbfb73981c38cd87703eeeeb8b05c9ea
feldblume5263 <feldblume5263@gmail.com>
2021-11-09 18:07:51 +0900

    init imageTest

---
+++ ImageTest/ImageTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageTest
+//
+//  Created by Junhong Park on 2021/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- swiftUI_tutorial/swiftUI_tutorial/Views/LandmarkList.swift
+++ swiftUI_tutorial/swiftUI_tutorial/Views/LandmarkList.swift
@@ -8,23 +8,44 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List(landmarks) {
-                landmark in NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
-                }
+            
+            List {
                 
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+                ForEach(filteredLandmarks) {
+                    landmark in NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+                .navigationTitle("Landmarks")
             }
-            .navigationTitle("Landmarks")
         }
+    }
+    
+    mutating func changValue() {
+        show
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkList()
+            .environmentObject(ModelData())
     }
 }
